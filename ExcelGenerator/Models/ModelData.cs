d0d7a2ef3d77bac432eef227218b00a4877ba5ac
Fernando Lima <fernando.aparecido@tivit.com>
2022-07-21 17:15:18 -0300

    Criação POC

---
+++ ExcelGenerator/Models/ModelData.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ExcelGenerator.Models
+{
+    public class ModelData
+    {
+        public string Subtitulo { get; set; }
+
+        [JsonProperty("paises")]
+        public List<Pais> Paises { get; set; }
+
+    }
+
+    public class Pais
+    {
+        [JsonProperty("gentilico")]
+        public string Gentilico { get; set; }
+
+        [JsonProperty("nome_pais")]
+        public string NomePais { get; set; }
+
+        [JsonProperty("nome_pais_int")]
+        public string NomePaisInt { get; set; }
+
+        [JsonProperty("sigla")]
+        public string Sigla { get; set; }
+    }
+
+}
---
+++ ExcelGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using ClosedXML.Report;
+using ExcelGenerator.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+
+namespace ExcelGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var subtitulo = "POC ClosedXML.Report";
+
+            var json = File.ReadAllText(@"..\..\..\Data\Paises.json");
+            var data = JsonConvert.DeserializeObject<ModelData>(json);
+            data.Subtitulo = subtitulo;
+
+            using (var template = new XLTemplate(@"..\..\..\Templates\Paises_Template.xlsx"))
+            {
+                template.AddVariable(data);
+                template.Generate();
+                template.SaveAs(@"..\..\..\Output\Paises.xlsx");
+            }
+       
+            //Show report
+            Process.Start(new ProcessStartInfo(@"..\..\..\Output\Paises.xlsx") { UseShellExecute = true });
+
+        }
+
+        /// <summary>
+        /// Retorno o array da bytes do excel.
+        /// </summary>
+        private static byte[] GetArrayBytesFromTamplate(XLTemplate template)
+        {
+            using (var ms = new MemoryStream())
+            {
+                template.Workbook.SaveAs(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                return ms.ToArray();
+            }
+        }
+    }
+}
